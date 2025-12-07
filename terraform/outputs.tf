5c62dfe2fe2514ef1bc974a9bcc2cd8967048d68
tienphatng237 <phat48419@gmail.com>
2025-12-07 15:26:36 +0700

    update(terraform): add ansible inventory formatted output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,2 +1,24 @@
-output "public_ips" { value = module.compute.public_ips }
-output "private_ips" { value = module.compute.private_ips }
+# Output raw lists
+output "public_ips" {
+  value = module.compute.public_ips
+}
+
+output "private_ips" {
+  value = module.compute.private_ips
+}
+
+# Output hosts.ini format cho Ansible
+output "ansible_inventory" {
+  value = <<-EOT
+    [controller]
+    controller-1 ansible_host=${module.compute.public_ips[0]} ansible_user=ubuntu private_ip=${module.compute.private_ips[0]}
+
+    [workers]
+    worker-1 ansible_host=${module.compute.public_ips[1]} ansible_user=ubuntu private_ip=${module.compute.private_ips[1]}
+    worker-2 ansible_host=${module.compute.public_ips[2]} ansible_user=ubuntu private_ip=${module.compute.private_ips[2]}
+    worker-3 ansible_host=${module.compute.public_ips[3]} ansible_user=ubuntu private_ip=${module.compute.private_ips[3]}
+
+    [all:vars]
+    ansible_ssh_private_key_file=../../key_pair/k0s_key
+  EOT
+}
